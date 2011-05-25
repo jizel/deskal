99f0c674f8c634a867103ffdbd2a56a9167a9083
drakoviens@gmail.com <drakoviens@gmail.com@50a34cfc-b265-c852-2d2b-7480995184a6>
2011-05-25 18:10:29 +0000

    iCal export

--- DesKal/src/cz/muni/fi/pb138/deskal/ExportImportImpl.java
+++ DesKal/src/cz/muni/fi/pb138/deskal/ExportImportImpl.java
@@ -1,8 +1,33 @@
 package cz.muni.fi.pb138.deskal;
 
 import java.io.File;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 public class ExportImportImpl implements ExportImport {
+
+    private String userDir = System.getProperty("user.home");
+    private String separator = System.getProperty("file.separator");
+    private String calendarXml = userDir + separator + "DesKal" + separator + "calendar.xml";
+    private static final String styleFile = "stylesheet/iCal.xsl";
+    private Transformer iCalTransformer;
+
+    public ExportImportImpl() {
+        TransformerFactory factory = TransformerFactory.newInstance();
+
+        Templates iCalExport;
+        try {
+            iCalExport = factory.newTemplates(new StreamSource(new File(styleFile)));
+            iCalTransformer = iCalExport.newTransformer();
+        } catch (TransformerConfigurationException ex) {
+            throw new RuntimeException("Error while building iCal transformer", ex);
+        }
+}
 
     public void importFromHCal(File file) {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -17,7 +42,10 @@ public class ExportImportImpl implements ExportImport {
     }
 
     public void exportToICal(File file) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            iCalTransformer.transform(new StreamSource(new File(calendarXml)), new StreamResult(file));
+        } catch (TransformerException ex) {
+            throw new RuntimeException("Error during transformation to iCal", ex);
+        }
     }
-
 }
